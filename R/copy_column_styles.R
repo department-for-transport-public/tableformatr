cbc43bdd5bbc78aab4fbbd9fae6ebfb81bf32d42
Fran <francesca.bryden@dft.gov.uk>
2022-09-07 16:26:57 +0100

    Vectorised function to accept non-continuous row and column arguments

--- R/copy_column_styles.R
+++ R/copy_column_styles.R
@@ -2,19 +2,20 @@
 #' @import openxlsx
 #' @name copyColumnStyles
 #' @param wb A Workbook object containing a worksheet.
-#' @param sheet The worksheet to write to. Can be the worksheet index or name.
-#' @param colFrom Integer showing the column number in the excel workbook of which the format is to be copied
-#' @param colTo An Integer defining the column number in the excel workbook for the format to be copied to
-#' @param startRow An integer indicating the row in the excel workbook to start copying the format
-#' @param endRow An integer indicating the row in the excel workbook to stop copying the format
-#' @title Copy the formatting of a column to another column for a defined consecutive selection of rows
+#' @param sheet The name of the worksheet to write to.
+#' @param colFrom Integer showing the column number in the excel workbook to 
+#' copy the formatting from 
+#' @param colTo An integer (or vector of integers) defining the column number 
+#' in the excel workbook for the format to be copied to
+#' @param rows A vector of one or more integer indicating the rows 
+#' in the excel workbook to copy the format from
+#' @title Copy the formatting of a column to another column for a defined selection of rows
 
 copyColumnStyles <- function(wb, 
                              sheet, 
                              colFrom, 
                              colTo, 
-                             startRow, 
-                             endRow) {
+                             rows) {
   
   #wb$styleObjects is a list containing lists containing style, rows and 
   #columns and sheet that style applies to
@@ -25,21 +26,18 @@ copyColumnStyles <- function(wb,
   ##Get styles for chosen cols and rows too
   all_styles <- all_styles[sapply(all_styles,
                                   function(x) colFrom %in% x$cols)]
-
-  #loop through each row in spreadsheet
-  for(row in startRow:endRow) {
-    #loop through each style in the workbook
-    for(i in 1:length(all_styles)) {
-      #if that style applies to the sheet and row looking at and the column copying from
-      if(row %in% all_styles[[i]]$rows) {
-        #Then apply the style to that sheet, row and column copying to
-        openxlsx::addStyle(wb,
-                           sheet, 
-                           all_styles[[i]]$style, 
-                           row, 
-                           colTo,
-                           stack = TRUE)
-      }
-    }
+  
+  #loop through each style in the workbook
+  for(i in 1:length(all_styles)) {
+    #For rows in both the style and the specified rows, apply the style
+    apply_rows <- intersect(rows, all_styles[[i]]$rows)
+    #Then apply the style to that sheet, copying to specified rows and columns
+    openxlsx::addStyle(wb,
+                       sheet = sheet,
+                       style = all_styles[[i]]$style,
+                       rows = apply_rows,
+                       cols = colTo,
+                       stack = TRUE,
+                       gridExpand = TRUE)
   }
 }
