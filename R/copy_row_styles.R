10efcb58d37cc6361791faea5c46e4c1d22c900e
AMIS-Sean-Longman <sean.longman@dft.gov.uk>
2023-09-01 09:47:04 +0100

    add copy row styles function

---
+++ R/copy_row_styles.R
@@ -0,0 +1,43 @@
+#' @export
+#' @import openxlsx
+#' @name copyRowStyles
+#' @param wb A Workbook object containing a worksheet.
+#' @param sheet The name of the worksheet to write to.
+#' @param rowFrom Integer showing the row number in the excel workbook to 
+#' copy the formatting from 
+#' @param rowTo An integer (or vector of integers) defining the row number 
+#' in the excel workbook for the format to be copied to
+#' @param cols A vector of one or more integer indicating the cols 
+#' in the excel workbook to copy the format from
+#' @title Copy the formatting of a row to another row for a defined selection of columns
+
+copyRowStyles <- function(wb, 
+                             sheet, 
+                             rowFrom, 
+                             rowTo, 
+                             cols) {
+  
+  #wb$styleObjects is a list containing lists containing style, rows and 
+  #columns and sheet that style applies to
+  #Get styles just from sheet of interest
+  all_styles <- wb$styleObjects[sapply(wb$styleObjects, 
+                                       function(x) x$sheet == sheet)]
+  
+  ##Get styles for chosen cols and rows too
+  all_styles <- all_styles[sapply(all_styles,
+                                  function(x) rowFrom %in% x$rows)]
+  
+  #loop through each style in the workbook
+  for(i in 1:length(all_styles)) {
+    #For rows in both the style and the specified rows, apply the style
+    apply_rows <- intersect(rows, all_styles[[i]]$rows)
+    #Then apply the style to that sheet, copying to specified rows and columns
+    openxlsx::addStyle(wb,
+                       sheet = sheet,
+                       style = all_styles[[i]]$style,
+                       rows = apply_rows,
+                       cols = colTo,
+                       stack = TRUE,
+                       gridExpand = TRUE)
+  }
+}
